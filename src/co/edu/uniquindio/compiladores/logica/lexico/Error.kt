551c2f37b49eff056554965cfcdd40289a7d672c
Daniel-Lp <danielalejandroloaizaperez@gmail.com>
2021-04-10 06:30:55 -0500

    Implementacion clase error y clase token

--- src/co/edu/uniquindio/compiladores/logica/lexico/Error.kt
+++ src/co/edu/uniquindio/compiladores/logica/lexico/Error.kt
@@ -1,4 +1,14 @@
 package co.edu.uniquindio.compiladores.logica.lexico
 
-class Error {
+import co.edu.uniquindio.compiladores.logica.lexico.Categoria
+
+/*
+Clase para reconocer los tokens del analizador lexico
+@autor Diego riveros, Stefanny Roman & Daniel Loaiza
+ */
+
+class Error (var error:String, var fila:Int, var columna:Int) {
+    override fun toString(): String {
+        return "Error(error='$error', fila=$fila, columna=$columna)"
+    }
 }
--- src/co/edu/uniquindio/compiladores/logica/lexico/Token.kt
+++ src/co/edu/uniquindio/compiladores/logica/lexico/Token.kt
@@ -1,4 +1,17 @@
 package co.edu.uniquindio.compiladores.logica.lexico
 
-class Token {
+import co.edu.uniquindio.compiladores.logica.lexico.Categoria
+
+/*
+Clase para reconocer los tokens del analizador lexico
+@autor Diego Diaz y Daniel Loaiza
+ */
+class Token(var lexema:String, var categoria: Categoria, var fila:Int, var columna:Int) {
+
+    /*
+    To String
+     */
+    override fun toString(): String {
+        return "Token ( lexema='$lexema', categoria=$categoria, fila=$fila, columna=$columna )"
+    }
 }
